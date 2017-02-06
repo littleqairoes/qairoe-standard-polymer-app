bcdacc0b93e56a454286368e1430093c690cb0cf
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-02-06 07:47:09 +0000

    Added splash page

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const gulpif = require('gulp-if');
+const watch = require('gulp-watch');
+const path = require('path');
+const injectSass = require('./gulp-tasks/inject-sass.js');
+const injectMetadata = require('./gulp-tasks/inject-metadata.js');
+/**
+ * This is a three part gulpfile.
+ *
+ * 1. SASS Strategy
+ * 2. Page Building Strategy
+ * 3. Production Building Strategy
+ */
+
+/**
+ * SASS STRATEGY
+ */
+
+gulp.task('watch-all', function(){
+  injectSass();
+  injectMetadata();
+  watch(['pages/**/*.scss', 'web-components/**/*.scss', 'styles/**/*.scss'], injectSass);
+  watch(['config/**/*.json'], injectMetadata);
+});
+
+gulp.task('inject-metadata', injectMetadata);
+gulp.task('inject-sass', injectSass);
